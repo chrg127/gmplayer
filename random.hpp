f3540ca9b2cc58a80b6dabfbd306bd82dab9a546
chrg <christian.ricci2000@gmail.com>
2022-07-14 08:03:30 +0200

    Add more functions to random.hpp

--- random.hpp
+++ random.hpp
@@ -1,27 +1,60 @@
 #pragma once
 
-#include <concept>
+#include <concepts>
+#include <cstddef>
+#include <algorithm>
 #include <random>
 #include <span>
+#include <vector>
 
-namespace random {
+namespace rng {
 
-inline std::mt19937 rng(std::random_device{}());
+inline thread_local auto seed = std::random_device{}();
+inline thread_local std::mt19937 rng(seed);
 
-template <typename T
+template <std::integral T = int>
+T get()
+{
+    std::uniform_int_distribution<T> dist;
+    return dist(rng);
+}
+
+template <std::floating_point T = float>
+T get()
+{
+    std::uniform_real_distribution<T> dist;
+    return dist(rng);
+}
+
+template <std::integral T = int>
 T between(T x, T y)
 {
     std::uniform_int_distribution<T> dist(x, y);
     return dist(rng);
 }
 
-template <typename T
-void shuffle(std::span<T> arr)
+template <std::floating_point T = float>
+T between(T x, T y)
+{
+    std::uniform_real_distribution<T> dist(x, y);
+    return dist(rng);
+}
+
+template <std::integral T = int>
+void shuffle_in_place(std::span<T> arr)
 {
     for (int i = 0; i < arr.size(); i++) {
-        int j = random_between(i, arr.size() - 1);
-        std::swap(arr[i], arr[j]);
+        std::swap(arr[i], arr[between<T>(0, arr.size() - 1)]);
     }
+}
+
+template <std::integral T = int>
+std::vector<int> shuffle(std::size_t count)
+{
+   std::vector<T> result(count);
+   std::iota(result.begin(), result.end(), 0);
+   shuffle_in_place<T>(result);
+   return result;
 }
 
 } // namespace random
