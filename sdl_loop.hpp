08076868b2c54e3a5fe2fe17107fed85694a0cb8
chrg <christian.ricci2000@gmail.com>
2022-07-14 08:02:03 +0200

    Remove file sdl_loop

--- sdl_loop.hpp
+++
@@ -1,9 +0,0 @@
-#include <QObject>
-
-class SDLEventLoop : public QObject {
-    Q_OBJECT
-public:
-    SDLEventLoop(QWidget *parent = nullptr) : QObject(parent) { }
-signals:
-    void ended(int r);
-};
